a5d4a6777b00a39eed3116941e7de2b657a428cd
Karachentsev Dmitry <xmitya@gmail.com>
2016-12-27 22:54:08 +0300

    setup

--- src/test/java/ru/ifmo/calculator/CalculatorTest.java
+++ src/test/java/ru/ifmo/calculator/CalculatorTest.java
@@ -7,37 +7,34 @@ import junit.framework.TestCase;
  */
 public class CalculatorTest extends TestCase {
 
-    public void testPlus() {
-        Calculator calc = new Calculator();
+    private Calculator calc;
 
+    @Override
+    protected void setUp() throws Exception {
+        calc = new Calculator();
+    }
+
+    public void testPlus() {
         assertEquals(7.0, calc.calculate("2 + 5"));
         assertEquals(10.0, calc.calculate("2 + 5 + 3"));
     }
 
     public void testMinus() {
-        Calculator calc = new Calculator();
-
         assertEquals(7.0, calc.calculate("10 - 3"));
         assertEquals(2.0, calc.calculate("10 - 3 - 5"));
     }
 
     public void testMultiply() {
-        Calculator calc = new Calculator();
-
         assertEquals(4.0, calc.calculate("2 * 2"));
         assertEquals(8.0, calc.calculate("4 * 2"));
     }
 
     public void testDivide() {
-        Calculator calc = new Calculator();
-
         assertEquals(4.0, calc.calculate("8 / 2"));
         assertEquals(2.5, calc.calculate("5 / 2"));
     }
 
     public void testComplex() {
-        Calculator calc = new Calculator();
-
         assertEquals(24.0, calc.calculate("2 + 4 * 6 - 10 / 5"));
     }
 }
